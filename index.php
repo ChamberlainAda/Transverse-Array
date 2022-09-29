0a0c9331838da876d5b99f55a7c4f06af15650cd
ChamberlainAda <79915937+ChamberlainAda@users.noreply.github.com>
2022-09-29 10:57:42 +0100

    First commit
    Solves the question one of the RECRUITMENT TEST

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+// CHAMBERLAIN-OKEZIE ADADEZE
+
+
+// Function to find insert position of K
+function checkArr($arr, $n , $K) {
+
+     // Traverse the array
+    for ($i = 0; $i < $n; $i++){
+        
+        // If K is found
+        if($arr[$i] == $K){
+            return $i;
+        }
+
+        // If current array element
+        // exceeds K
+        else if ($arr[$i] > $K) {  
+            return $i;
+        }
+
+    }
+     // If all elements are smaller
+    // than K
+    return $n;
+}
+
+// Given the array 'arr' to insert the integer 'K, in order , 'K' would have to be placed at index 1
+$arr = array(1,3,5,6);
+$n = count($arr);
+$K = 2;
+
+echo(checkArr($arr, $n, $K));
+?>
